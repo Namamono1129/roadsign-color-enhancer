970c7c1ef0ea4ccd9cc7c665eb9b864b531dfb79
namamono1129 <namamono1129v@gmail.com>
2018-08-29 15:42:58 +0900

    Edited comment.

--- white_balance.cpp
+++ white_balance.cpp
@@ -12,37 +12,37 @@ using namespace cv;
 /*** public method ***/
 void FixWhiteBalance(const Mat src_image, Mat& dest_image)
 {
-	/*** チャネル分割 ***/
+	/*** Divide RGB Channels ***/
 	vector<Mat> channels;
 	cv::split(src_image, channels);
 
-	/*** パラメータ(設定改善の余地あり/オケーションに合わせる) ***/
 	float mean_intensity = cv::mean(src_image.reshape(1))[0];
+	// This parameter has more room for improvement
 	float portion = mean_intensity * mean_intensity / 120000.0;
 
-	// 先にこのへんの値を計算しておくことで高速化する
+	// For performance reason, calculate them first:
 	const int num_bins = 256;
-	const int num_pixels = src_image.rows * src_image.cols;	// ヒストグラムの全ビンの合計値と同じ
+	const int num_pixels = src_image.rows * src_image.cols;	// Make sure this number is equal to the number of bins in histogram
 	const int histSize[] = { num_bins, num_pixels };
 	const float range[] = { 0, 255 };
 	const float* ranges[] = { range, range, range };
 	const int channel_to_calc[] = { 0 };
 	
-	// ワンループで全処理を済ませたいのでチャネルごと処理をまとめている
 	for (int i = 0; i < 3; i++)
 	{
-		/*** ヒストグラム計算 ***/
+		/*** Calculating (channel's) histogram ***/
 		Mat hist;
 		cv::calcHist(&channels[i], 1, channel_to_calc, Mat(),
 			hist, 1, histSize, ranges,
 			true, false);
 
-		/*** ヒストグラムから上界と下界を求める ***/
+		/*** Get lower/upper bound of original image from histogram ***/
 		uchar lower_bound = CalcLowerBound(hist, num_pixels, portion);
 		uchar higher_bound = CalcUpperBound(hist, num_pixels, portion);
 
 		float scale_factor = (255.0 / (higher_bound - lower_bound));
 
+		/*** Expanding histogram ***/
 		cv::subtract(channels[i], lower_bound, channels[i]);
 		cv::multiply(channels[i], scale_factor, channels[i]);
 	}
